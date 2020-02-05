6a38e0dbe33214200bb46d6ffabb187f8315ecae
James Benjamin <james.abenj@gmail.com>
2020-02-05 01:10:25 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents
-  def change 
+  def change
   add_column :students, :grade, :integer 
-  add_column :students, :birthdate, :string 
-  end 
+  add_column :students, :birthdate, :string
+  end
 end
